40ef3042b7a0c3868319d8228ef228cf843b663e
Mitchell Stanton-Cook <mscook@gmail.com>
2014-01-29 09:26:19 +1000

    Tweaking install process

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
+import ez_setup
+ez_setup.use_setuptools()
+
 from setuptools import setup, find_packages
 from os.path import join, dirname
+import beb
 
 
 setup(
-    name='beb',
-    version='0.1',
-    packages=find_packages(),
-    long_description=open(join(dirname(__file__), 'README.rst')).read(),
+    name             = 'Banzai-EasyBuild',
+    version          = beb.__version__,
+    author           = "Mitchell Stanton-Cook",
+    author_email     = "m.stantoncook@gmail.com",
+    description      = ("Banzai-EasyBuild (BEB) is for installing 3rd party "
+                        "Banzai NGS pipeline dependencies"),
+    packages         = find_packages(),
+    long_description = open(join(dirname(__file__), 'README.rst')).read(),
+    url              = "https://github.com/mscook/Banzai-EasyBuild",
+    install_requires = [
+        'easybuild==1.10.0'
+        ]
 )
