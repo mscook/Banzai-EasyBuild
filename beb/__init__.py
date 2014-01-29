6843c2a858a1b05118e8ca2406157c759cb2bed9
Mitchell Stanton-Cook <mscook@gmail.com>
2014-01-29 11:33:20 +1000

    Biolerplate

--- beb/__init__.py
+++ beb/__init__.py
@@ -1,1 +1,8 @@
-__version__ = '0.1.0'
+__title__        = 'Banzai-EasyBuild (BEB)'
+__version__      = '0.1.0'
+__description__  = ("A no fuss next-generation sequencing analysis stack "
+                    "bootstrapping tool")
+__author__       = 'Mitchell Stanton-Cook'
+__license__      = 'ECL 2.0'
+__author_email__ = "m.stantoncook@gmail.com"
+__url__          = 'https://github.com/mscook/Banzai-EasyBuild'
